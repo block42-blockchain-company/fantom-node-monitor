0a9068476eba092569035f74f88063a8dc67fa47
DollyVolley <oliver.schwarz@block42.tech>
2020-07-13 17:16:26 +0200

    Stackswitch InfluxDB --> Prometheus

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import subprocess
-from influxdb import InfluxDBClient
 from common import consts
 
 print("==== Fantom Node Monitor Setup ====\n")
@@ -10,43 +9,25 @@ network_setup = subprocess.Popen(command.split())
 command = "docker volume create grafana-volume"
 grafana_volume_creation = subprocess.Popen(command.split())
 
-command = "docker volume create influxdb-volume"
-influxdb_volume_creation = subprocess.Popen(command.split())
+command = "docker volume create prometheus-volume"
+prometheus_volume_creation = subprocess.Popen(command.split())
 
 grafana_volume_creation.wait()
 network_setup.wait()
-influxdb_volume_creation.wait()
+prometheus_volume_creation.wait()
 
 
-print("\n==== Initialize InfluxDB ====")
-command = '''docker run --rm -e INFLUXDB_HTTP_AUTH_ENABLED=true \
-         -e INFLUXDB_ADMIN_USER={} \
-         -e INFLUXDB_ADMIN_PASSWORD={} \
-         -v /var/lib/influxdb:/var/lib/influxdb \
-         -v /etc/influxdb/scripts:/docker-entrypoint-initdb.d \
-         influxdb /init-influxdb.sh '''.format(consts.INFLUXDB_ADMIN_USER, consts.INFLUXDB_ADMIN_PASSWORD)
-init_influxdb = subprocess.Popen(command.split())
-init_influxdb.wait()
-
-
-print("\n==== Start Grafana and InfluxDB Container ====")
+print("\n==== Start Grafana and Prometheus Container ====")
 command = 'docker-compose up -d'
 spin_up_containers = subprocess.Popen(command.split())
 spin_up_containers.wait()
 
-
-print("\n==== Create Database ====")
-client = InfluxDBClient(host="localhost",
-                        port=8086,
-                        username=consts.INFLUXDB_ADMIN_USER,
-                        password=consts.INFLUXDB_ADMIN_PASSWORD)
-client.create_database(consts.DATABASE_NAME)
-
-
-print("\n==== Start Monitoring Service ====")
+#print("\n==== Start Monitoring Service ====")
 # Note: the script below is only for basic testing and has nothing to do with the actual monitoring!
-command = "python3 ./monitor.py"
-create_db = subprocess.Popen(command.split())
+#command = "python3 ./monitor.py"
+#create_db = subprocess.Popen(command.split())
+
+command = "wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz"
 
 
 print("\n==== Setup complete! ====")
--- tear_down.py
+++ tear_down.py
@@ -12,11 +12,11 @@ rm_network = subprocess.Popen(command.split())
 command = "docker volume rm grafana-volume"
 rm_grafana_volume = subprocess.Popen(command.split())
 
-command = "docker volume rm influxdb-volume"
-rm_influxdb_volume = subprocess.Popen(command.split())
+command = "docker volume rm prometheus-volume"
+rm_prometheus_volume = subprocess.Popen(command.split())
 
 rm_network.wait()
 rm_grafana_volume.wait()
-rm_influxdb_volume.wait()
+rm_prometheus_volume.wait()
 
 print("Teardown complete!")
