87134a36479266f6d542bffc6f3c16cbf72b514d
DollyVolley <oliver.schwarz@block42.tech>
2020-07-13 16:22:02 +0200

    Minor Fix

--- monitor.py
+++ monitor.py
@@ -9,19 +9,17 @@ client = InfluxDBClient(host="localhost",
                         username=consts.INFLUXDB_ADMIN_USER,
                         password=consts.INFLUXDB_ADMIN_PASSWORD)
 
-def getMetric(metric):
-    return [
-    {
+def getMetric():
+    return [{
         "measurement": consts.TABLE_NAME,
         "fields": {
             "percentage": 0.64,
             "status": "synced"
         }
-    }
-]
+    }]
 
 def storeMetricToDB(db, payload):
     client.write_points(payload, database=consts.DATABASE_NAME, protocol='json')
 
 
-storeMetricToDB(consts.DATABASE_NAME, getMetric)
+storeMetricToDB(consts.DATABASE_NAME, getMetric())
