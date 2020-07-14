6f0c0ee49e5344360546de79a3b08c0e3a3ea777
DollyVolley <oliver.schwarz@block42.tech>
2020-07-14 12:09:38 +0200

    Update README

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # Fantom Node Monitor
 
+The Fantom Node Monitor is currently in a mvp version.
+It consists of a Grafana Dashboard, which will display the metrics
+gained from Prometheus. Both are running in a docker container and 
+using the host network. This may change in future versions.
+<br>
+By spinning up the node monitor, all configurations and dashboards are 
+created for you. 
+<br>
+<br>
+Note: Since this is a minimal version we have currently added the 
+external module <a src="https://github.com/prometheus/node_exporter">node_exporter</a> to Prometheus as a data source.
+Fantom specific metrics will follow as soon as the exporter for it is ready.
+
 ## Getting started
-
-### Prerequists
-Make sure that the package manager for python3 is installed. You can do that via
-
-```    
-sudo apt-get install python3-pip
-```
-Make sure all dependenices are installed:
-
-```
-pip3 install -r requirements.txt
-```
 
 ### Setup
 
--- tear_down.py
+++ tear_down.py
@@ -1,5 +1,8 @@
 import subprocess
+import os
+import signal
 from common import consts
+
 
 print("==== Fantom Node Monitor Teardown ====\n")
 
@@ -7,8 +10,16 @@ command = "docker-compose down"
 shutdown_container = subprocess.Popen(command.split())
 shutdown_container.wait()
 
-command = "lsof -ti tcp:{} | xargs kill".format(consts.NODE_EXPORTER_PORT)
-shutdown_node_exporter = subprocess.Popen(command.split())
+command = "lsof -ti :{}".format(consts.NODE_EXPORTER_PORT)
+shutdown_node_exporter = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+stdout, stderr = shutdown_node_exporter.communicate()
+print(stdout.decode("utf-8"))
+for process in str(stdout.decode("utf-8")).split("\n")[1:]:
+    data = [x for x in process.split(" ") if x != '']
+    if len(data) > 1:
+        print("Killing {}".format(data))
+        os.kill(int(data[1]), signal.SIGKILL)
 
 command = "docker volume rm grafana-volume"
 rm_grafana_volume = subprocess.Popen(command.split())
