ad847ff9fb35961583b744bbcec61f227421537e
DollyVolley <oliver.schwarz@block42.tech>
2020-07-15 13:30:24 +0200

    Fix Teardown

--- tear_down.py
+++ tear_down.py
@@ -7,11 +7,8 @@ def shutDownProcessByPort(port):
     stdout, stderr = shutdown_node_exporter.communicate()
     pid = stdout.decode("utf-8")
 
-    print(pid)
-    if pid.isdigit():
-        print("Kill PID: {}".format(pid))
-        command = "kill {}".format(pid)
-        subprocess.Popen(command.split())
+    command = "kill {}".format(pid)
+    subprocess.Popen(command.split())
 
 
 print("==== Fantom Node Monitor Teardown ====\n")
@@ -20,15 +17,14 @@ command = "docker-compose down"
 shutdown_container = subprocess.Popen(command.split())
 shutdown_container.wait()
 
-shutDownProcessByPort(consts.NODE_EXPORTER_PORT)
-shutDownProcessByPort(consts.LACHESIS_EXPORTER_PORT)
-
-
 command = "docker volume rm grafana-volume"
 rm_grafana_volume = subprocess.Popen(command.split())
 
 command = "docker volume rm prometheus-volume"
 rm_prometheus_volume = subprocess.Popen(command.split())
+
+shutDownProcessByPort(consts.NODE_EXPORTER_PORT)
+shutDownProcessByPort(consts.LACHESIS_EXPORTER_PORT)
 
 rm_grafana_volume.wait()
 rm_prometheus_volume.wait()
