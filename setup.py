e45935e66bd62cdd65a95471b522be3ee339aa40
DollyVolley <oliver.schwarz@block42.tech>
2020-08-04 15:54:29 +0200

    Try node_exporter as docker container

--- setup.py
+++ setup.py
@@ -25,25 +25,13 @@ print("\n==== Start Lachesis_Exporter ====")
 command = "./{}/lachesis_exporter/lachesis_exporter_linux_amd64".format(consts.BINARY_FOLDER)
 start_lachesis_exporter = subprocess.Popen(command.split())
 
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-if not os.path.exists(dir_path + consts.BINARY_FOLDER + consts.NODE_EXPORTER_TAR_FILE):
-    print("\n==== Download Node_Exporter ====")
-
-    print("Downloading Node_Exporter 1.0.1")
-    command = \
-        "wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz" \
-        " -P ./{}".format(consts.BINARY_FOLDER)
-    download_node_exporter = subprocess.Popen(command.split())
-    download_node_exporter.wait()
-
-    command = "tar -xzf ./{}/{} -C {}".format(consts.BINARY_FOLDER, consts.NODE_EXPORTER_TAR_FILE, consts.BINARY_FOLDER)
-    unwrap_node_exporter = subprocess.Popen(command.split())
-    unwrap_node_exporter.wait()
-
-
 print("\n==== Start Node_Exporter ====")
-command = "./{}/node_exporter-1.0.1.linux-amd64/node_exporter".format(consts.BINARY_FOLDER)
+command = 'docker run -d \
+  --net="host" \
+  --pid="host" \
+  -v "/:/host:ro,rslave" \
+  quay.io/prometheus/node-exporter \
+  --path.rootfs=/host'
 start_node_exporter = subprocess.Popen(command.split())
 
 print("\n==== Setup complete! ====")
