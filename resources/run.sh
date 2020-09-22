0afbd0e7fee901e84d61ed85a24a8cbd33691a01
tomazmm <tomaz.mesarec@live.com>
2020-09-22 11:42:19 +0200

    Monitor exporters: * Check if all services are still running

--- resources/run.sh
+++ resources/run.sh
@@ -9,14 +9,13 @@ if [ $status -ne 0 ]; then
 fi
 
 
-# Start the lachesis exporter
-#lachesis_exporter &
-#status=$?
-#echo "Status", $status
-#if [ $status -ne 0 ]; then
-#  echo "Failed to start my_second_process: $status"
-#  # exit $status
-#fi
+#Start the lachesis exporter
+lachesis_exporter &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start my_second_process: $status"
+  # exit $status
+fi
 
 # Start the grafana server
 service grafana-server start &
@@ -43,19 +42,23 @@ fi
 # if it detects that either of the processes has exited.
 # Otherwise it loops forever, waking up every 60 seconds
 
-while sleep 5; do
+while sleep 30; do
   ps aux |grep node_exporter |grep -q -v grep
-  PROCESS_1_STATUS=$?
+  NODE_EXPORTER_STATUS=$?
   ps aux |grep prometheus |grep -q -v grep
-  PROCESS_2_STATUS=$?
+  PROMETHEUS_STATUS=$?
   ps aux |grep grafana-server |grep -q -v grep
-  PROCESS_3_STATUS=$?
-  echo "Node_exporter: $PROCESS_1_STATUS"
-  echo "prometheus: $PROCESS_2_STATUS"
-  echo "grafana: $PROCESS_3_STATUS"
+  GRAFANA_STATUS=$?
+  ps aux |grep lachesis_exporter |grep -q -v grep
+  LACHESIS_EXPORTER_STATUS=$?
+  echo "Node_exporter: $NODE_EXPORTER_STATUS"
+  echo "Prometheus: $PROMETHEUS_STATUS"
+  echo "Grafana: $GRAFANA_STATUS"
+  echo "Lachesis: $LACHESIS_EXPORTER_STATUS"
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+  if [ $NODE_EXPORTER_STATUS -ne 0 -o $LACHESIS_EXPORTER_STATUS -ne 0 ] ||
+     [ $GRAFANA_STATUS -ne 0 -o $PROMETHEUS_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
     exit 1
   fi
