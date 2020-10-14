bba89749328afc943f8bb90e6e36444efd620df0
tomazmm <tomaz.mesarec@live.com>
2020-10-14 11:49:10 +0200

    If container exists already, teardown and set it up again

--- setup.py
+++ setup.py
@@ -3,31 +3,31 @@ from tear_down import *
 
 
 def setup():
-    try:
-        print("==== Fantom Node Monitor Setup ====\n")
+    print("==== Fantom Node Monitor Setup ====\n")
 
-        print("Create persistent volumes:")
+    print("Create persistent volumes:")
 
-        command = "docker volume create prometheus-volume"
-        prometheus_volume_creation = subprocess.Popen(command.split())
+    command = "docker volume create prometheus-volume"
+    prometheus_volume_creation = subprocess.Popen(command.split())
 
-        prometheus_volume_creation.wait()
+    prometheus_volume_creation.wait()
 
-        print("Start container")
+    print("Start container")
 
-        # "host" attributes necessary in order for node_exporter to access host machine metrics
-        command = 'docker run -d \
-          --net=host \
-          --pid=host \
-          -v /:/host:ro,rslave \
-          -v prometheus-volume:/data \
-          --name fantom-node-monitor \
-          block42blockchaincompany/fantom-node-monitor'
+    # "host" attributes necessary in order for node_exporter to access host machine metrics
+    command = 'docker run -d \
+      --net=host \
+      --pid=host \
+      -v /:/host:ro,rslave \
+      -v prometheus-volume:/data \
+      --name fantom-node-monitor \
+      block42blockchaincompany/fantom-node-monitor'
 
-        node_monitor_startup = subprocess.run(command.split())
-        node_monitor_startup.check_returncode()
-    except subprocess.CalledProcessError as e:
-        print(e.output)
+    node_monitor_startup = subprocess.run(command.split(), stdout=subprocess.PIPE,
+                                          universal_newlines=True, stderr=subprocess.PIPE)
+
+    err_msg = node_monitor_startup.stderr
+    if err_msg and "is already in use" in err_msg:
         tear_down()
         setup()
 
