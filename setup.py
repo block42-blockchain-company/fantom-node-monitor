ae0fb13e2b16425b7a20af331d0719d79d2836bb
tomazmm <tomaz.mesarec@live.com>
2020-10-14 11:05:59 +0200

    Test me remote pls

--- setup.py
+++ setup.py
@@ -2,28 +2,31 @@ import subprocess
 
 
 def setup():
-    print("==== Fantom Node Monitor Setup ====\n")
+    try:
+        print("==== Fantom Node Monitor Setup ====\n")
 
-    print("Create persistent volumes:")
+        print("Create persistent volumes:")
 
-    command = "docker volume create prometheus-volume"
-    prometheus_volume_creation = subprocess.Popen(command.split())
+        command = "docker volume create prometheus-volume"
+        prometheus_volume_creation = subprocess.Popen(command.split())
 
-    prometheus_volume_creation.wait()
+        prometheus_volume_creation.wait()
 
-    print("Start container")
+        print("Start container")
 
-    # "host" attributes necessary in order for node_exporter to access host machine metrics
-    command = 'docker run -d \
-      --net=host \
-      --pid=host \
-      -v /:/host:ro,rslave \
-      -v prometheus-volume:/data \
-      --name fantom-node-monitor \
-      block42blockchaincompany/fantom-node-monitor'
+        # "host" attributes necessary in order for node_exporter to access host machine metrics
+        command = 'docker run -d \
+          --net=host \
+          --pid=host \
+          -v /:/host:ro,rslave \
+          -v prometheus-volume:/data \
+          --name fantom-node-monitor \
+          block42blockchaincompany/fantom-node-monitor'
 
-    node_monitor_startup = subprocess.Popen(command.split())
-    node_monitor_startup.wait()
+        node_monitor_startup = subprocess.Popen(command.split())
+        node_monitor_startup.wait()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
